0c9a82869a7b6b372d4cf975cd65174822f5e1db
jean Carlos Heck <jeancarlosheck@gmail.com>
2026-05-19 19:09:38 -0300

    Adding GameHub and GameStateStore tests

---
+++ Tests/Unit/Events/Hubs/GameHubTests.cs
@@ -0,0 +1,30 @@
+namespace Tests.Unit.Events.Hubs;
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Moq;
+using Xunit;
+using Backend.Events.Hubs;
+using Backend.Events.Services;
+
+public class GameHubTests
+{
+    [Fact]
+    public async Task OnConnectedAsync_ShouldDispatchInitialStateToConnectedClient()
+    {
+        string expectedConnectionId = "test-connection-id-12345";
+        Mock<IEventDispatcherService> eventDispatcherMock = new Mock<IEventDispatcherService>();
+        Mock<HubCallerContext> hubCallerContextMock = new Mock<HubCallerContext>();
+        
+        hubCallerContextMock.Setup(context => context.ConnectionId).Returns(expectedConnectionId);
+
+        GameHub gameHub = new GameHub(eventDispatcherMock.Object)
+        {
+            Context = hubCallerContextMock.Object
+        };
+
+        await gameHub.OnConnectedAsync();
+
+        eventDispatcherMock.Verify(dispatcher => dispatcher.DispatchInitialStateToClient(expectedConnectionId), Times.Once);
+    }
+}
---
+++ Tests/Unit/Events/States/GameStateStoreTests.cs
@@ -0,0 +1,34 @@
+namespace Tests.Unit.Events.States;
+
+using Xunit;
+using Backend.Events.States;
+using Backend.Domain.Models;
+
+public class GameStateStoreTests
+{
+    [Fact]
+    public void UpdateState_ShouldUpdateCurrentState()
+    {
+        GameStateStore gameStateStore = new GameStateStore();
+        State expectedState = new State();
+
+        gameStateStore.UpdateState(expectedState);
+
+        Assert.NotNull(gameStateStore.CurrentState);
+        Assert.Same(expectedState, gameStateStore.CurrentState);
+    }
+
+    [Fact]
+    public void ClearState_ShouldResetCurrentStateAndSetEmulatorConnectedToFalse()
+    {
+        GameStateStore gameStateStore = new GameStateStore();
+        State dummyState = new State();
+        gameStateStore.UpdateState(dummyState);
+        gameStateStore.IsEmulatorConnected = true;
+
+        gameStateStore.ClearState();
+
+        Assert.Null(gameStateStore.CurrentState);
+        Assert.False(gameStateStore.IsEmulatorConnected);
+    }
+}
