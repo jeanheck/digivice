4a2525b472b239e542403b95aa64b06785b00830
jean Carlos Heck <jeancarlosheck@gmail.com>
2026-05-22 22:26:55 -0300

    Creating new translating structure

---
+++ Frontend/src/i18n/locales/en-US/index.ts
@@ -0,0 +1,13 @@
+import common from "./common.json";
+import gameplay from "./gameplay.json";
+import battle from "./battle.json";
+import digimon from "./digimon.json";
+import stats from "./stats.json";
+
+export default {
+  ...common,
+  ...gameplay,
+  ...battle,
+  ...digimon,
+  ...stats
+};
