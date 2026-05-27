8f6ebe3855ca63c5604387a763a1692f8b33059d
jean Carlos Heck <jeancarlosheck@gmail.com>
2026-05-27 19:47:22 -0300

    Refactoring Map components, phase 1

--- Frontend/src/presenters/map.presenter.ts
+++ Frontend/src/presenters/map.presenter.ts
@@ -1,8 +1,14 @@
+import { EnemyRepository } from "@/repositories/enemy.repository";
 import { LocationRepository } from "@/repositories/location.repository";
+import type { EnemyResumedViewModel } from "@/viewmodels/enemy/enemy-resumed.viewmodel";
 import type { LocationViewModel } from "@/viewmodels/location/location.viewmodel";
 
 export class MapPresenter {
     public static getLocationById(locationId: string): LocationViewModel {
         return LocationRepository.getLocationById(locationId);
+    }
+
+    public static getResumedEnemiesByIds(enemyIds: string[]): EnemyResumedViewModel[] {
+        return enemyIds.map((enemyId) => EnemyRepository.getResumedEnemyById(enemyId));
     }
 }
