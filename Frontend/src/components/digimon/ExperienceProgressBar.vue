4e7e752c864671190f8361e26637e1afcc6e3ffa
jean Carlos Heck <jeancarlosheck@gmail.com>
2026-05-27 14:58:13 -0300

    remove useless component

--- Frontend/src/components/digimon/ExperienceProgressBar.vue
+++ Frontend/src/components/digimon/ExperienceProgressBar.vue
@@ -8,13 +8,10 @@ defineProps<{
 
 <template>
   <div class="relative w-full h-6 bg-[#000e3f] rounded overflow-hidden shadow-inner flex items-center justify-center border-2 border-[#00154a]">
-    <!-- Animated Progress Bar -->
     <div 
       class="absolute left-0 top-0 h-full transition-all duration-500 bg-opacity-90 bg-linear-to-r from-orange-600 to-yellow-500"
       :style="{ width: `${experiencePercentageToReachNextLevel}%` }"
     ></div>
-    
-    <!-- Text Overlay -->
     <span class="relative z-10 text-[0.6rem] font-bold text-white drop-shadow-md px-1 tracking-wider">
       EXP: {{ experience }} / {{ experienceToReachNextLevel }}
     </span>
