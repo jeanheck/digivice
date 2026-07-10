d1df71fc2dcb864630efe85f8800c233fd20ef67
jean Carlos Heck <jeancarlosheck@gmail.com>
2026-07-10 19:41:24 -0300

    Updating docks coordinates

--- Frontend/src/components/map/map-modal/Dock.vue
+++ Frontend/src/components/map/map-modal/Dock.vue
@@ -55,7 +55,17 @@ watch(
     class="relative shrink-0 max-h-full min-h-0 overflow-y-auto overflow-x-hidden custom-scroll bg-[#00051a] border border-cyan-800/50 rounded shadow-[0_0_15px_rgba(0,170,255,0.1)]"
     :style="{ width: `${MAP_FRAME_WIDTH_PX}px` }"
   >
-    <div class="relative overflow-hidden" :style="mapImageFrameStyle">
+    <div
+      class="relative overflow-hidden flex items-center justify-center"
+      :style="mapImageFrameStyle"
+    >
+      <p
+        v-if="!imageUrl"
+        class="text-cyan-500/50 text-sm tracking-widest text-center px-8"
+      >
+        {{ $t("mapModal.noDock") }}
+      </p>
+
       <img
         v-if="imageUrl"
         :key="imageUrl"
@@ -66,9 +76,12 @@ watch(
 
       <div
         v-if="coordinates"
-        class="absolute z-10 w-3.5 h-3.5 rounded-full bg-cyan-500/50 -translate-x-1/2 -translate-y-1/2 pointer-events-none"
+        class="absolute z-10 w-12 h-12 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center pointer-events-none"
         :style="{ left: coordinates.x + '%', top: coordinates.y + '%' }"
-      />
+      >
+        <div class="absolute inset-0 rounded-full border border-cyan-400 animate-ping opacity-90" />
+        <div class="w-4 h-4 rounded-full bg-cyan-400 shadow-[0_0_8px_rgba(0,255,255,1)]" />
+      </div>
     </div>
   </div>
 </template>
