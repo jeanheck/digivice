3826163c79c3cd3840c9a78b133aa86873703b4b
jean Carlos Heck <jeancarlosheck@gmail.com>
2026-07-10 13:14:06 -0300

    Small style changes

--- Frontend/src/components/party/digimon/profile/DigievolutionsButton.vue
+++ Frontend/src/components/party/digimon/profile/DigievolutionsButton.vue
@@ -13,7 +13,7 @@ function onClick(): void {
 
 <template>
   <div
-    class="w-20 h-6 bg-[#000e3f] border-2 border-[#00154a] rounded flex items-center justify-center pt-0.2 cursor-pointer hover:bg-[#001233] transition-colors"
+    class="w-20 h-6 bg-[#000e3f] border-2 border-[#00154a] rounded flex items-center justify-center pt-0.2 cursor-pointer hover:bg-[#002a77] transition-colors"
     @click="onClick"
     @mouseenter="emit('showTooltip', $event)"
     @mousemove="emit('moveTooltip', $event)"
