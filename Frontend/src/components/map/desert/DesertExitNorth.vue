875c25ef5b5696a824d11f7e4cfae4bf3957292c
jean Carlos Heck <jeancarlosheck@gmail.com>
2026-07-17 00:12:54 -0300

    Trying to adjust emoji and text position

--- Frontend/src/components/map/desert/DesertExitNorth.vue
+++ Frontend/src/components/map/desert/DesertExitNorth.vue
@@ -5,8 +5,8 @@ defineProps<{
 </script>
 
 <template>
-  <div v-if="name" class="absolute top-2 right-2 z-20 flex items-center gap-1.5">
+  <div v-if="name" class="absolute top-2 right-2 z-20 max-w-[90%]">
     <span class="text-[10px] text-yellow-300 text-right leading-tight whitespace-nowrap">{{ name }}</span>
-    <span class="inline-flex leading-none text-[1.2rem] -translate-y-0.5 filter-[sepia(1)_saturate(6)_hue-rotate(5deg)_brightness(1.05)]">↗️</span>
+    <span class="inline-flex leading-none text-[1.2rem] filter-[sepia(1)_saturate(6)_hue-rotate(5deg)_brightness(1.05)]">↗️</span>
   </div>
 </template>
