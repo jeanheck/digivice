b8bfe493289ed5edf56209741934ddb16125b66f
jean Carlos Heck <jeancarlosheck@gmail.com>
2026-07-16 23:56:03 -0300

    Adjusting color and styles

--- Frontend/src/components/map/desert/DesertExitWest.vue
+++ Frontend/src/components/map/desert/DesertExitWest.vue
@@ -5,8 +5,8 @@ defineProps<{
 </script>
 
 <template>
-  <div v-if="name" class="absolute top-2 left-2 z-20 flex items-center gap-1.5 max-w-[45%]">
-    <span class="inline-flex leading-none text-[1.2rem] -translate-y-0.5">↖️</span>
-    <span class="text-[9px] text-yellow-300 text-left leading-tight">{{ name }}</span>
+  <div v-if="name" class="absolute top-2 left-2 z-20 flex items-center gap-1.5">
+    <span class="inline-flex leading-none text-[1.2rem] -translate-y-0.5 filter-[sepia(1)_saturate(6)_hue-rotate(5deg)_brightness(1.05)]">↖️</span>
+    <span class="text-[10px] text-yellow-300 text-left leading-tight whitespace-nowrap">{{ name }}</span>
   </div>
 </template>
