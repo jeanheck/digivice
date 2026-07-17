2b3a73ed42d96b7f33fdbef716135a48320ab4bb
jean Carlos Heck <jeancarlosheck@gmail.com>
2026-07-17 00:19:05 -0300

    Final adjusts

--- Frontend/src/components/map/desert/DesertExitWest.vue
+++ Frontend/src/components/map/desert/DesertExitWest.vue
@@ -5,7 +5,7 @@ defineProps<{
 </script>
 
 <template>
-  <div v-if="name" class="absolute top-2 left-2 z-20">
+  <div v-if="name" class="absolute top-2 left-2 z-20 max-w-[45%]">
     <span class="inline-flex leading-none text-[1.2rem] filter-[sepia(1)_saturate(6)_hue-rotate(5deg)_brightness(1.05)]">↖️</span>
     <span class="text-[10px] text-yellow-300 text-left leading-tight">{{ name }}</span>
   </div>
