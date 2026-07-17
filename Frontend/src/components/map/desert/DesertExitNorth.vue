b2835aa97d7cba3a3970702547f261dfb479933e
jean Carlos Heck <jeancarlosheck@gmail.com>
2026-07-17 10:00:45 -0300

    Show text of North in one line

--- Frontend/src/components/map/desert/DesertExitNorth.vue
+++ Frontend/src/components/map/desert/DesertExitNorth.vue
@@ -5,7 +5,7 @@ defineProps<{
 </script>
 
 <template>
-  <div v-if="name" class="absolute top-2 right-2 z-20 max-w-[45%]">
+  <div v-if="name" class="absolute top-2 right-2 z-20 whitespace-nowrap">
     <span class="text-[10px] text-yellow-300 text-right leading-tight">{{ name }}</span>
     <span class="inline-flex leading-none text-[1.2rem] filter-[sepia(1)_saturate(6)_hue-rotate(5deg)_brightness(1.05)]">↗️</span>
   </div>
