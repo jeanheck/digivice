19e0595cb2f2f1a4bfefc256b5721a996d3ad0ac
jean Carlos Heck <jeancarlosheck@gmail.com>
2026-07-17 10:27:47 -0300

    Adding more zoomed locations to some side quests

--- Frontend/src/components/map/desert/DesertExitNorth.vue
+++ Frontend/src/components/map/desert/DesertExitNorth.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
   <div v-if="name" class="absolute top-2 right-2 z-20 flex items-baseline">
-    <span class="max-w-[160px] whitespace-normal text-[10px] text-yellow-300 text-right leading-tight">{{ name }}</span>
+    <span class="max-w-40 whitespace-normal text-[10px] text-yellow-300 text-right leading-tight">{{ name }}</span>
     <span class="inline-flex shrink-0 leading-none text-[1.2rem] filter-[sepia(1)_saturate(6)_hue-rotate(5deg)_brightness(1.05)]">↗️</span>
   </div>
 </template>
