a3b4077b5374761d738c3b33229228c8a26ed99d
jean Carlos Heck <jeancarlosheck@gmail.com>
2026-07-17 09:55:18 -0300

    Refactoring desert directions

--- Frontend/src/components/map/desert/DesertExitNorth.vue
+++ Frontend/src/components/map/desert/DesertExitNorth.vue
@@ -5,8 +5,8 @@ defineProps<{
 </script>
 
 <template>
-  <div v-if="name" class="absolute top-2 right-2 z-20 flex w-[45%] items-start justify-end gap-1.5 text-right">
-    <span class="min-w-0 text-balance text-[10px] text-yellow-300 leading-tight">{{ name }}</span>
-    <span class="inline-flex shrink-0 leading-none text-[1.2rem] filter-[sepia(1)_saturate(6)_hue-rotate(5deg)_brightness(1.05)]">↗️</span>
+  <div v-if="name" class="absolute top-2 right-2 z-20 max-w-[45%]">
+    <span class="text-[10px] text-yellow-300 text-right leading-tight">{{ name }}</span>
+    <span class="inline-flex leading-none text-[1.2rem] filter-[sepia(1)_saturate(6)_hue-rotate(5deg)_brightness(1.05)]">↗️</span>
   </div>
 </template>
