cde439428b163bf31f3b957e9b73e73c5d51bdc8
jean Carlos Heck <jeancarlosheck@gmail.com>
2026-07-21 22:27:15 -0300

    Refactoring SeabedMap

--- Frontend/src/components/map/SeabedMap.vue
+++ Frontend/src/components/map/SeabedMap.vue
@@ -24,21 +24,17 @@ const mapVariant = computed(() => {
   return store.currentState?.player?.mapVariant ?? 0;
 });
 
-const locationViewModel = computed(() => {
-  if (locationId.value === null) {
-    return null;
-  }
-
-  return SeabedMapPresenter.getLocation(locationId.value, seabedRoute.value);
+const enemyIds = computed(() => {
+  return SeabedMapPresenter.getEnemyIds(seabedRoute.value);
 });
 </script>
 
 <template>
   <div class="relative z-10 flex flex-col flex-1 min-h-0 pt-1">
     <div class="flex flex-col items-center gap-2 shrink-0">
-      <Location :location-id="locationViewModel?.id ?? null" />
+      <Location :location-id="locationId" />
       <Enemies
-        :enemy-ids="locationViewModel?.enemies ?? []"
+        :enemy-ids="enemyIds"
         @open-enemy-modal="emit('open-enemy-modal', $event)"
       />
       <Seabed
--- Frontend/src/presenters/map/seabed-map.presenter.ts
+++ Frontend/src/presenters/map/seabed-map.presenter.ts
@@ -1,13 +1,7 @@
-import { LocationConverter } from "@/presenters/converter/location.converter";
-import { LocationRepository } from "@/repositories/location.repository";
 import { LocationService } from "@/services/location.service";
-import type { LocationViewModel } from "@/viewmodels/location/location.viewmodel";
 
 export class SeabedMapPresenter {
-  public static getLocation(locationId: string, seabedRoute: number = 0): LocationViewModel {
-    const locationRaw = LocationRepository.getLocationById(locationId);
-    const enemyIds = LocationService.getSeabedEnemies(seabedRoute);
-
-    return LocationConverter.convert(locationId, locationRaw, enemyIds);
+  public static getEnemyIds(seabedRoute: number): string[] {
+    return LocationService.getSeabedEnemies(seabedRoute);
   }
 }
