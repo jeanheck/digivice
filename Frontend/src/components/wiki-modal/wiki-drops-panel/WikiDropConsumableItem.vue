d3222edd1c25924916cb7be5d00f531699398cec
jean Carlos Heck <jeancarlosheck@gmail.com>
2026-08-18 20:05:48 -0300

    Finishing first version of WikiDropConsumableItem

--- Frontend/src/components/wiki-modal/wiki-drops-panel/WikiDropConsumableItem.vue
+++ Frontend/src/components/wiki-modal/wiki-drops-panel/WikiDropConsumableItem.vue
@@ -44,30 +44,48 @@ const additionalInformation = computed(() => {
 </script>
 
 <template>
-  <div v-if="consumableItem" class="flex flex-col min-h-0 overflow-y-auto custom-scroll text-xs">
-    <p>
-      <span class="text-blue-500 uppercase">{{ $t("enemy.resaleValueLabel") }}: </span>
-      <span class="text-gray-100">{{ consumableItem.resaleValue }}&nbsp;</span>
-      <span class="text-blue-500 uppercase">{{ $t("player.bits") }}</span>
-    </p>
-    <p class="mt-2">
-      <span class="text-blue-500 uppercase">{{ $t("enemy.soldInStore") }}: </span>
-      <span class="text-gray-100 uppercase">
-        {{ consumableItem.soldInStore ? $t("enemy.soldInStoreYes") : $t("enemy.soldInStoreNo") }}
-      </span>
-    </p>
+  <div
+    v-if="consumableItem"
+    class="flex flex-col min-h-0 overflow-y-auto custom-scroll text-xs text-center gap-4"
+  >
+    <div class="flex flex-col gap-1">
+      <p class="bg-[#002266]/40 px-2 py-1 rounded-sm text-blue-500 uppercase font-bold">
+        {{ $t("enemy.resaleValueLabel") }}
+      </p>
+      <p>
+        <span class="text-gray-100 uppercase font-bold">{{ consumableItem.resaleValue }}&nbsp;</span>
+        <span class="text-blue-500 uppercase font-bold">{{ $t("player.bits") }}</span>
+      </p>
+    </div>
 
-    <p
-      v-if="note !== ''"
-      class="mt-4 text-gray-400 italic leading-tight"
+    <div class="flex flex-col gap-1">
+      <p class="bg-[#002266]/40 px-2 py-1 rounded-sm text-blue-500 uppercase font-bold">
+        {{ $t("enemy.soldInStore") }}
+      </p>
+      <p
+        class="uppercase font-bold"
+        :class="consumableItem.soldInStore ? 'text-green-400' : 'text-red-400'"
+      >
+        {{ consumableItem.soldInStore ? $t("enemy.soldInStoreYes") : $t("enemy.soldInStoreNo") }}
+      </p>
+    </div>
+
+    <div
+      v-if="note !== '' || additionalInformation !== ''"
+      class="flex flex-col gap-4"
     >
-      {{ note }}
-    </p>
-    <p
-      v-if="additionalInformation !== ''"
-      class="mt-4 text-violet-500 leading-tight"
-    >
-      {{ additionalInformation }}
-    </p>
+      <p
+        v-if="note !== ''"
+        class="text-violet-400 font-bold"
+      >
+        {{ note }}
+      </p>
+      <p
+        v-if="additionalInformation !== ''"
+        class="text-gray-400 italic leading-tight"
+      >
+        {{ additionalInformation }}
+      </p>
+    </div>
   </div>
 </template>
