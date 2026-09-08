05629036e5228058d8ffea8fea5e3b4d14ab9dac
jean Carlos Heck <jeancarlosheck@gmail.com>
2026-09-08 20:36:52 -0300

    Adding reverse info on consumable items

--- Frontend/src/repositories/tables/raws/consumable-item/consumable-item.raw.ts
+++ Frontend/src/repositories/tables/raws/consumable-item/consumable-item.raw.ts
@@ -1,4 +1,11 @@
+export interface ConsumableItemDroppedByRaw {
+  kind: "enemy";
+  id: string;
+  locationOnly?: string;
+}
+
 export interface ConsumableItemRaw {
   resaleValue: number;
   soldInStore: boolean;
+  droppedBy: ConsumableItemDroppedByRaw[];
 }
